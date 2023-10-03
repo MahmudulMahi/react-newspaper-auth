0b36627bed47e5276b38b593f2ec5436b9f136f0
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-10-04 01:09:04 +0600

    creat all caterory and icon

--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,9 +1,48 @@
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from "react-icons/fa6";
+import qZone1 from '../../../assets/qZone1.png'
+import qZone2 from '../../../assets/qZone2.png'
+import qZone3 from '../../../assets/qZone3.png'
 
 
 const RightSideNav = () => {
   return (
     <div>
-      <h2 className="text-2xl"> right side</h2>
+      <div className="p-4 space-y-3 mb-6">
+        <h2 className="text-3xl"> Login</h2>
+        <button className="btn btn-outline w-full">
+          <FaGoogle></FaGoogle>
+          Google
+        </button>
+        <button className="btn btn-outline w-full">
+          <FaGithub></FaGithub>
+          GitHub
+        </button>
+      </div>
+
+      <div className="p-4  mb-6">
+        <h2 className="text-3xl"> Find Us On</h2>
+
+        <a className="p-4 flex text-lg items-center border rounded-t-lg" href="">
+          <FaFacebook className="mr-3"></FaFacebook>
+          Facebook
+        </a>
+        <a className="p-4 flex text-lg items-center border-x " href="">
+          <FaTwitter className="mr-3"></FaTwitter>
+          Twitter
+        </a>
+        <a className="p-4 flex text-lg items-center border rounded-b-lg" href="">
+          <FaInstagram className="mr-3"></FaInstagram>
+          Instagram
+        </a>
+        {/* q zone */}
+        <div className="p-4 space-y-3 mb-6">
+          <h2 className="text-3xl"> Q-zone</h2>
+        <img src={qZone1} alt="" />
+        <img src={qZone2} alt="" />
+        <img src={qZone3} alt="" />
+        </div>
+
+      </div>
     </div>
   );
 };
--- src/pages/Shared/leftSidenav/leftSidenav.jsx
+++ src/pages/Shared/leftSidenav/leftSidenav.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const LeftSidenav = () => {
+  const [categories,setCategories]=useState([])
+
+  useEffect(()=>{
+    fetch("categories.json")
+    .then(res =>res.json())
+    .then(data => setCategories(data) )
+  },[])
   return (
-    <div>
-      <h2 className="text-2xl">left leftSidenav</h2>
+    <div className="space-y-6">
+      <h2 className="text-2xl">All Caterogy</h2>
+      {
+        categories.map(category=> <Link 
+          className=" block ml-4 text-xl font-semibold" 
+        key={category.id}
+        to={`/category/${category.id}`}
+        >{category.name}</Link>)
+      }
     </div>
   );
 };
